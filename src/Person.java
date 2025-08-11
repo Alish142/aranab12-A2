2e182dbd2d387fa9e21c74d94a356ea745c1221f
Alish <a.ranabhat.12@student.scu.edu.au>
2025-08-11 15:35:01 +1000

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -14,4 +14,20 @@ public class Person {
         this.age       = age;
     }
 
+    //getters and Setters 
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " (age " + age + ")";
+    }
+
 }
