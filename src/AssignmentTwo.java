2c70af5bb56bb4d20306d0c1741d84e61acb98cd
Alish <a.ranabhat.12@student.scu.edu.au>
2025-08-11 16:21:39 +1000

    Update AssignmentTwo.java

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -1,3 +1,23 @@
 public class AssignmentTwo {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Alish", "Ranabhat", 25, "E102", "Ride Operator", true);
+        Ride coaster = new Ride("Hyper Coaster", 130, employee);
+        Visitor visitor = new Visitor("Ayush", "Rana", 18, "T001", false);
+
+
+        System.out.println(employee);
+        System.out.println(coaster);
+        System.out.println(visitor);
+
+    }
+
+    // placeholders for other parts . 
+        public void partThree(){ }
+    public void partFourA(){ }
+    public void partFourB(){ }
+    public void partFive(){ }
+    public void partSix(){ }
+    public void partSeven(){ }
+    
     
 }
