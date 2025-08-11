6385d82647c01d4f2cad1afb114c2b1d31d7f409
Alish <a.ranabhat.12@student.scu.edu.au>
2025-08-11 15:53:35 +1000

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,33 @@
-public class Employee {
-    
+public class Employee extends Person {
+    private String staffId;
+    private String role;     
+    private boolean onDuty;
+
+   // Default constructor
+    public Employee() { }
+
+    // Paramaterised constructer 
+
+    public Employee(String firstName, String lastName, int age,
+                    String staffId, String role, boolean onDuty) {
+        super(firstName, lastName, age);
+        this.staffId = staffId;
+        this.role    = role;
+        this.onDuty  = onDuty;
+    }
+
+    //getters and setters for Emlpyee
+    public String getStaffId() { return staffId; }
+    public void setStaffId(String staffId) { this.staffId = staffId; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+
+    public boolean isOnDuty() { return onDuty; }
+    public void setOnDuty(boolean onDuty) { this.onDuty = onDuty; }
+
+    @Override
+    public String toString() {
+        return "Employee[" + super.toString() + ", staffId=" + staffId + ", role=" + role + ", onDuty=" + onDuty + "]";
+    }
 }
