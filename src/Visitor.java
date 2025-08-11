66616dc5c44388592d2b32852053eb37d872c899
Alish <a.ranabhat.12@student.scu.edu.au>
2025-08-11 16:03:06 +1000

    Update Visitor.java

--- src/Visitor.java
+++ src/Visitor.java
@@ -1,3 +1,28 @@
-public class Visitor {
+public class Visitor extends Person {
+    private String ticketId;
+    private boolean fastPass;
+
+    //Default Constructor 
+    public Visitor(){}
+
+    //Paramatersied constructor 
+    public Visitor(String firstName, String lastName, int age,
+    String ticketId, boolean fastPass) {
+super(firstName, lastName, age);
+this.ticketId = ticketId;
+this.fastPass = fastPass;
+}
+
+// getters and setters 
+public String getTicketId() { return ticketId; }
+    public void setTicketId(String ticketId) { this.ticketId = ticketId; }
+
+    public boolean isFastPass() { return fastPass; }
+    public void setFastPass(boolean fastPass) { this.fastPass = fastPass; }
+
+    @Override
+    public String toString() {
+        return "Visitor[" + super.toString() + ", ticketId=" + ticketId + ", fastPass=" + fastPass + "]";
+    }
     
 }
